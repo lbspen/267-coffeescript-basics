e6c4dc65b0829e0b7c242c9ecd687f26ba8503e2
lbs <lbspen@gmail.com>
2013-06-12 22:09:14 -0700

    change orders.js to use coffeescript

--- store-before/db/schema.rb
+++ store-before/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20110523034722) do
   create_table "orders", :force => true do |t|
     t.string   "credit_card_number"
     t.date     "credit_card_expires_on"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
   end
 
 end
